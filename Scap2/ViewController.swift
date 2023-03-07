33da14bc4f5dc3531e274f351858ebdd7e9f5948
Md. Mehedi Hasan <mehedi@traaal.com>
2023-03-08 02:53:47 +0600

    initial

---
+++ Scap2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scap2
+//
+//  Created by Md. Mehedi Hasan on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
